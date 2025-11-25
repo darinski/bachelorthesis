44d695c8a2604ef865367bf8e4e5d58d91ebe509
darinski <daria.shevyrev@gmail.com>
2025-11-25 15:38:05 +0100

    changing p

--- src/offline.py
+++ src/offline.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 
 DEFAULT_SCALE = 2**16
-DEFAULT_P = 2**64 - 59 
+DEFAULT_P = 1009
 DEFAULT_TEST_SIZE = 0.2
 OUTPUT_DIR = "Player-Data"
 DEFAULT_BATCH_SIZE = 1
